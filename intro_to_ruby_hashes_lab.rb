b854f1e8c5a3fb0e8f92a0dd71313f6d34961767
Mitchell T Alton <mitchell.alton@live.com>
2020-01-06 16:58:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,9 +19,6 @@ def monopoly_with_second_tier
       :three_pieces_owned => 100,
       :four_pieces_owned => 200
     },
-    :names => {
-      
-    },
   },
 	  
 	}
@@ -35,6 +32,11 @@ def monopoly_with_third_tier
       :two_pieces_owned => 50,
       :three_pieces_owned => 100,
       :four_pieces_owned => 200
+    },
+    :names => {
+      :reading_railroad => {},
+      :pennsylvania_railroad => {},
+      
     },
   },
 	  
