de0d6e238839526a76ca77d8748805b904e7daea
Mitchell T Alton <mitchell.alton@live.com>
2020-01-06 17:05:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -58,7 +58,7 @@ def monopoly_with_fourth_tier
         :mortgage_value => "$" + 100
       },
       :pennsylvania_railroad => {
-        :mortgage_value => "$"100
+        
       },
       :b_and_o_railroad => {},
       :shortline_railroad => {}
