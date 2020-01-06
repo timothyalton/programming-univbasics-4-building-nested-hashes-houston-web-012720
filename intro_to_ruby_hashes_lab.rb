cf8de2f59ac54b8022a8265aa888da93fbc90775
Mitchell T Alton <mitchell.alton@live.com>
2020-01-06 16:19:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,8 +14,10 @@ def monopoly_with_second_tier
   {:railroads => {
     :pieces => 4,
     :rent_in_dollars => {
-      :one_piece_owned => 50,
-      :two_piece
+      :one_piece_owned => 25,
+      :two_pieces_owned => 50,
+      :three_pieces_owned => 100,
+      :four_pieces_owned => 200
     },
   },
 	  
